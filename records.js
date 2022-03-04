719624ad97a8112e8d6e063ea4895b87abd30bcc
Glitch (umbrella-fm) <none>
2022-03-04 02:09:20 +0000

    🍓🐍 Updated with Glitch

--- records.js
+++ records.js
@@ -12,47 +12,5 @@ const records =  [
     "desc": "Thunder and lightening storm passing over town, rain on buildings, some traffic noise but mainly drowned by thunder. Recorded in North America Summer 1989. <a href='https://sound-effects.bbcrewind.co.uk/search?q=NHU05075001'>bbc.co.uk – © copyright BBC</a>",
     "kind" : "rain",
     "link" : "https://cdn.glitch.global/d0348cf0-2764-43de-96b5-8d953d48957d/BBCNigelThunderstorm1989.m4a?v=1644576318354"
-  },
-  //      {
-  //   "name": "<a href='https://hoot.alex.gd'>Alex Tomlinson</a>",
-  //   "trak": "Spitfire Espresso",
-  //   "desc": "Stumbled across a busy cafe in Glasgow, Scotland while walking toward the Necropolis. Recorded Winter 2021.",
-  //   "kind" : "cafe",
-  //   "link" : "https://cdn.glitch.global/d0348cf0-2764-43de-96b5-8d953d48957d/2021.27.11%20-%20Spitfire%20Espresso-looped.m4a?v=1644577118307"
-  // },
-         {
-    "name": "<a href='https://aporee.org/maps/?loc=29355&snd=33780'>rmacmich</a>",
-    "trak": "Nighttime sounds",
-    "desc": "Sounds of a summer night in Leesburg, VA. Recorded 2015.",
-    "kind" : "other",
-    "link" : "https://cdn.glitch.global/19a7949e-6c54-4e8b-b924-67de709f344b/Nighttime20sounds2020Leesburg20VA20202015081620201233_01.m4a?v=1644752203677"
-  },
-       {
-    "name": "<a href='https://aporee.org/maps/?loc=2490&snd=3477'>DROR</a>",
-    "trak": "internet cafe + online gambling",
-    "desc": "Recorded 2008 in Stockholm.",
-    "kind" : "cafe",
-    "link" : "https://cdn.glitch.global/d0348cf0-2764-43de-96b5-8d953d48957d/netzcafe_01.m4a?v=1644618089875"
-  },
-  //        {
-  //   "name": "<a href='https://aporee.org/maps/?loc=55299&m=satellite'>sonoquilibrium</a>",
-  //   "trak": "IN THE ATTIC DURING THE CHRISTMAS DAY",
-  //   "desc": "Recorded 2021 in Berlin.",
-  //   "kind" : "rain",
-  //   "link" : "https://cdn.glitch.global/19a7949e-6c54-4e8b-b924-67de709f344b/NAPODS1_01.m4a?v=1644749904501"
-  // },
-         {
-    "name": "<a href='https://hoot.alex.gd'>Alex Tomlinson</a>",
-    "trak": "Deacon's House Cafe",
-    "desc": "A nice little cafe in Edinburgh, Scotland on the Royal Mile in Old Town. Recorded Winter 2021.",
-    "kind" : "cafe",
-    "link" : "https://cdn.glitch.global/d0348cf0-2764-43de-96b5-8d953d48957d/DeaconsHouseCafe-looped.m4a?v=1644578345915"
-  },
-           {
-    "name": "<a href='https://hoot.alex.gd'>Alex Tomlinson</a>",
-    "trak": "Mystery Cassette 01",
-    "desc": "Ambient excerpts from a personal cassette recorder, most likely recorded around 2013 in and around Scranton, Pennsylvania.",
-    "kind" : "other",
-    "link" : "https://cdn.glitch.global/d0348cf0-2764-43de-96b5-8d953d48957d/1_01.m4a?v=1644597423394"
   }
 ]
