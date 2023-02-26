dc680e2f08cbd61ff56f8affe8c5f53e31f32206
Glitch (umbrella-fm) <none>
2023-02-26 02:14:56 +0000

    🥒🌦️ Updated with Glitch

--- records.js
+++ records.js
@@ -12,5 +12,12 @@ const records =  [
     "desc": "Thunder and lightening storm passing over town, rain on buildings, some traffic noise but mainly drowned by thunder. Recorded in North America Summer 1989. <a href='https://sound-effects.bbcrewind.co.uk/search?q=NHU05075001'>bbc.co.uk – © copyright BBC</a>",
     "kind" : "rain",
     "link" : "https://cdn.glitch.global/d0348cf0-2764-43de-96b5-8d953d48957d/BBCNigelThunderstorm1989.m4a?v=1644576318354"
+  },
+       {
+    "name": "<a href='https://links.alex.gd'>Alex Tomlinson</a>",
+    "trak": "Heavy Rain Paris Apartment",
+    "desc": "Recorded in December 2021 inside a Parisian apartment in the evening.",
+    "kind" : "rain",
+    "link" : "https://cdn.glitch.global/19a7949e-6c54-4e8b-b924-67de709f344b/2021.12.28%20-%20Pouring%20Rain%20in%20Evening%20Apartment%20-%20u09wj8smjfnf_01.mp3?v=1677377349229"
   }
 ]
